c3b8b8c4a0a0d8ce2af558f272cc769415e1e6a8
parsa <parsakhosravani@gmail.com>
2023-08-19 15:21:30 +0330

    docker issue Nodejs

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# Getting started
+# Welcome to Docker
 
-This repository is a sample application for users following the getting started guide at https://docs.docker.com/get-started/.
+This is a repo for new users getting started with Docker.
 
-The application is based on the application from the getting started tutorial at https://github.com/docker/getting-started
+You can try it out using the following command.
+```
+docker run -d -p 8088:80 --name welcome-to-docker docker/welcome-to-docker
+```
+And open `http://localhost:8088` in your browser.
+
+# Building
+
+Maintainers should see [MAINTAINERS.md](MAINTAINERS.md).
+
+Build and run:
+```
+docker build -t welcome-to-docker . 
+docker run -d -p 8088:3000 --name welcome-to-docker welcome-to-docker
+```
+Open `http://localhost:8088` in your browser.
--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-const express = require('express');
-const app = express();
-const db = require('./persistence');
-const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
-const updateItem = require('./routes/updateItem');
-const deleteItem = require('./routes/deleteItem');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-app.use(express.json());
-app.use(express.static(__dirname + '/static'));
-
-app.get('/items', getItems);
-app.post('/items', addItem);
-app.put('/items/:id', updateItem);
-app.delete('/items/:id', deleteItem);
-
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
-
-const gracefulShutdown = () => {
-    db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
-};
-
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
